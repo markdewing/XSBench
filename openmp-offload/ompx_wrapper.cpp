1db312f07c8d0f888f437e73da9492657d45bd06
Mark Dewing <markdewing@gmail.com>
2023-08-07 18:21:42 -0500

    More cleanup of ompx_wrapper

--- openmp-offload/ompx_wrapper.cpp
+++ openmp-offload/ompx_wrapper.cpp
@@ -1,16 +1,11 @@
 
-#include <ompx/sort.h>
+#include <ompx/sort_by_key.h>
 
 // C interface
 extern "C" {
 void ompx_device_sort_by_key(int* keys_first, int* keys_last, double* values_first)
 {
     ompx::device::sort_by_key(keys_first, keys_last, values_first);
-}
-
-void ompx_device_sort_by_key_n(int* keys_first, uint32_t NumElements, double* values_first)
-{
-    ompx::device::sort_by_key(keys_first, NumElements, values_first);
 }
 }
 
--- openmp-offload/ompx_wrapper.h
+++ openmp-offload/ompx_wrapper.h
@@ -3,7 +3,6 @@
 extern "C" {
 #endif
 void ompx_device_sort_by_key(int* keys_first, int* keys_last, double* values_first);
-void ompx_device_sort_by_key_n(int* keys_first, uint32_t len, double* values_first);
 
 #ifdef __cplusplus
 }
